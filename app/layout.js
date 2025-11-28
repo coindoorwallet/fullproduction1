a37f53d921764141d0ee525534a6397d6fcdf41c
coindoorwallet <coindoorwallet@gmail.com>
2025-11-28 22:19:09 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,13 @@
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
-export const metadata = {
-  title: "CoinDoor",
-  description: "Crypto markets, news & education",
-};
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-black text-white">
         <Navbar />
-        <div className="pt-20">{children}</div>
+        <main className="container min-h-screen">{children}</main>
         <Footer />
       </body>
     </html>
