e669a854754410ad71e7551383754e737f8f6b50
coindoorwallet <coindoorwallet@gmail.com>
2025-11-29 00:08:50 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
-import "./globals.css";
 import Navbar from "./components/Navbar";
-
-export const metadata = {
-  title: "CoinDoor",
-  description: "Market intelligence for modern crypto investors",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">
+      <body>
         <Navbar />
         {children}
       </body>
