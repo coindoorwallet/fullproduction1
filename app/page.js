c4077d16c2ec60981230af9dd939e03178bab73e
coindoorwallet <coindoorwallet@gmail.com>
2025-12-05 21:50:26 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,40 @@
+"use client";
+
 import Navbar from "./components/Navbar";
 import Ticker from "./components/Ticker";
 import Hero from "./components/Hero";
 import TopCoins from "./components/TopCoins";
 import TopNews from "./components/TopNews";
+
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "Arial, sans-serif", background: "#000", color: "#fff" }}>
+      
+      {/* NAVBAR */}
+      <section>
+        <Navbar />
+      </section>
+
+      {/* LIVE TICKER */}
+      <section style={{ marginTop: "20px" }}>
+        <Ticker />
+      </section>
+
+      {/* HERO SECTION */}
+      <section style={{ marginTop: "40px" }}>
+        <Hero />
+      </section>
+
+      {/* TOP COINS */}
+      <section style={{ marginTop: "50px" }}>
+        <TopCoins />
+      </section>
+
+      {/* TOP NEWS */}
+      <section style={{ marginTop: "50px", marginBottom: "50px" }}>
+        <TopNews />
+      </section>
+
+    </main>
+  );
+}
