bce181939e1864c18ac19d17078fd87f292e596a
coindoorwallet <coindoorwallet@gmail.com>
2025-12-05 21:48:19 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,50 +1,5 @@
-import Navbar from "@/components/Navbar";
-import Ticker from "@/components/Ticker";
-import Hero from "@/components/Hero";
-import TopCoins from "@/components/TopCoins";
-import TopNews from "@/components/TopNews";
-import EducatorProgram from "@/components/EducatorProgram";
-import CommunitySection from "@/components/CommunitySection";
-import ResearchSection from "@/components/ResearchSection";
-import Spotlight from "@/components/Spotlight";
-import JoinTeam from "@/components/JoinTeam";
-import Footer from "@/components/Footer";
-
-export default function Home() {
-  return (
-    <main className="w-full min-h-screen bg-black text-white flex flex-col items-center">
-      {/* NAVIGATION */}
-      <Navbar />
-
-      {/* TICKER */}
-      <Ticker />
-
-      {/* HERO */}
-      <Hero />
-
-      {/* TOP COINS */}
-      <TopCoins />
-
-      {/* TOP NEWS */}
-      <TopNews />
-
-      {/* EDUCATOR PROGRAM */}
-      <EducatorProgram />
-
-      {/* COMMUNITY */}
-      <CommunitySection />
-
-      {/* RESEARCH */}
-      <ResearchSection />
-
-      {/* HUMPHREY'S SPOTLIGHT */}
-      <Spotlight />
-
-      {/* JOIN OUR TEAM */}
-      <JoinTeam />
-
-      {/* FOOTER */}
-      <Footer />
-    </main>
-  );
-}
+import Navbar from "./components/Navbar";
+import Ticker from "./components/Ticker";
+import Hero from "./components/Hero";
+import TopCoins from "./components/TopCoins";
+import TopNews from "./components/TopNews";
